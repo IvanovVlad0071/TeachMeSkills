6a9351cf01020dd5f2f5de5985458491836fb61c
Ivanov Vlad <ivanov.vlad.0071@gmail.com>
2023-05-10 21:34:30 +0300

    homework3

---
+++ task_2.ts
@@ -0,0 +1,14 @@
+//Создайте переменные:
+
+//- количество секунд в минуте
+//- количество минут в часу
+//- количество часов в сутках
+//- количество суток в году
+
+//Посчитайте ваш возраст в секундах и поместите результат в переменную **`myAgeInSeconds`**
+let SecInMin = 60;
+let MinInHour = 60;
+let HourInDay = 24;
+let DayInYear = 365;
+let myAgeInSeconds = SecInMin*MinInHour*HourInDay*DayInYear*28;
+console.log(myAgeInSeconds)  
---
+++ task_3.ts
@@ -0,0 +1,17 @@
+//Создайте две переменные. Поместите в них переменную **`count`** и превратите в строку, а **`userName`** наоборот в число. 
+//Попробуйте реализовать задачу двумя разными способами.
+    //let count = 42
+    // let a = String(count);
+    //console.log(typeof a)
+    
+    //let userName = '42'
+    //let b = Number(userName);
+    //console.log(typeof b)
+
+   // let count = 42;
+   // let c = count.toString();
+   // console.log(typeof c);
+
+   let username = '42';
+   let d = parseInt(username);
+   console.log(typeof d);
